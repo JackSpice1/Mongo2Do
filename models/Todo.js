d5e63be5e2be640e32c918118089b166066d543e
Jackie Spiess <Jackiespiess22@gmail.com>
2023-03-02 10:08:41 -0600

    API creation

---
+++ models/Todo.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+	text: {
+		type: String,
+		required: true
+	},
+	complete: {
+		type: Boolean,
+		default: false
+	},
+	timestamp: {
+		type: String,
+		default: Date.now()
+	},
+	category: {
+		type: String,
+	},
+	duedate:{
+		type: String,
+	}
+});
+
+const Todo = mongoose.model("Todo", TodoSchema);
+
+module.exports = Todo;
